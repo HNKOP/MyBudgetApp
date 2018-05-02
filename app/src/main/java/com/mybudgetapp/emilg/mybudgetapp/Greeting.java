a7035f42cfc0c77968f005fe97f9a5bd3666b832
HNKOP <emilgasanov@mail.ru>
2018-05-02 19:34:41 +0300

    Рабочая отправка Get-запросов

---
+++ app/src/main/java/com/mybudgetapp/emilg/mybudgetapp/Greeting.java
@@ -0,0 +1,15 @@
+package com.mybudgetapp.emilg.mybudgetapp;
+
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
--- app/src/main/java/com/mybudgetapp/emilg/mybudgetapp/MainActivity.java
+++ app/src/main/java/com/mybudgetapp/emilg/mybudgetapp/MainActivity.java
@@ -1,7 +1,16 @@
 package com.mybudgetapp.emilg.mybudgetapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPushOnClick(View view) {
+        Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_SHORT).show();
+        new HttpRequestTask().execute();
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
+        @Override
+        protected Greeting doInBackground(Void... params) {
+            try {
+                final String url = "http://rest-service.guides.spring.io/greeting";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                
+                Greeting greeting = restTemplate.getForObject(url,Greeting.class);
+
+                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            TextView greetingIdText = (TextView) findViewById(R.id.textView);
+            greetingIdText.setText(greeting.getContent());
+        }
+
     }
 }
